53503697c95f1da9895e1a7761b69f5c34b86c3a
Jeff <jeff.sagal@briteweb.com>
2019-10-06 12:59:28 -0700

    init

---
+++ Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <span v-html="icon"></span>
+</template>
+
+<script>
+  import icons from 'octicons'
+
+  export default {
+    props: ['name'],
+    computed: {
+      icon () {
+        return icons.hasOwnProperty(this.name) ? icons[this.name].toSVG() : ''
+      }
+    }
+  }
+</script>
+
+<style type="text/scss" scoped>
+  span {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
